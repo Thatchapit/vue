e504917ecf04d05b9b0e6f638370833a35e0253d
gee <lnwgee@gmail.com>
2017-04-05 15:02:56 +0700

    finish

--- app-demo/src/components/Hello.vue
+++ app-demo/src/components/Hello.vue
@@ -2,8 +2,8 @@
 <div class="hello">
   <h1>{{ msg }}</h1>
 
-  <input placeholder="Firstname Lastname" v-model="detail" />
-    <h2>{{detail}}</h2>
+  <input placeholder="Search by name" v-model="name" />
+    <h2>{{name}}</h2>
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
@@ -12,15 +12,15 @@
           <thead>
             <tr>
               <th>#No.</th>
-              <th>First Name</th>
+              <th>Image</th>
               <th>Eng Name</th>
               <th>Date Time</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(item,count) in items" style="text-align:left">
+            <tr v-for="item in filteredItems" style="text-align:left">
               <th>{{item.id}}</th>
-
+              <td><img class="img-responsive" :src="imagePath(item.id)"></td>
               <td>{{item.ename}}</td>
               <td>{{item.base.HP}}</td>
             </tr>
@@ -34,45 +34,34 @@
 </template>
 
 <script>
-import  apiURL  from '../assets/pokedex.json'
 
+import  apiURL from '../assets/pokedex.json'
 export default {
-  name: 'hello',
   data() {
     return {
       msg: 'Welcome to Your Vue.js App',
-      detail: '',
-      items: []
+      name: '',
+      items: apiURL,
+
     }
   },
   created: function(){
-    this.fetchData();
+
+  },
+  computed: {
+    filteredItems() {
+      var that = this;
+
+
+      return that.items.filter(value => {
+
+         return value.ename.toLowerCase().indexOf(that.name.toLowerCase()) >= 0 
+      })
+    }
   },
   methods: {
-    // pressed() {
-    //
-    //   if (this.detail !== "") {
-    //     let name = []
-    //     let date = new Date().toISOString().slice(0, 10);
-    //
-    //     name = this.detail.split(" ");
-    //     this.items.push({
-    //       firstname: name[0],
-    //       lastname: name[1],
-    //       time: date
-    //     });
-    //     this.detail = "";
-    //   }else{
-    //     alert("Input Your Name!");
-    //   }
-    // },
-    fetchData() {
-
-    var self = this;
-    $.get( apiURL, function( data ) {
-      console.log(data);
-     self.items = data;
-     });
+    imagePath: function (path) {
+      return require('../assets/spr/'+path+'MS.png')
     }
   }
 }
